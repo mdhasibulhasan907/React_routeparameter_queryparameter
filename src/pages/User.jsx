ec7d893047c71211c35310d39e75ade73b49ff9e
Md. Hasibul Hasan <abeer.rocky@gmail.com>
2023-06-26 13:19:56 +0600

    query Parameter modify

--- src/pages/User.jsx
+++ src/pages/User.jsx
@@ -1,14 +1,40 @@
 // eslint-disable-next-line no-unused-vars
 import React from 'react'
+import { useState } from 'react';
 import { useSearchParams} from 'react-router-dom'
 const User = () => {
+  const [name,setName]=useState("");
   // eslint-disable-next-line no-unused-vars
   const [search,setSearch]=useSearchParams();
+
+
+
+
+const handleSubmit=(e)=>{
+ e.preventDefault();
+
+ setSearch({name:name})
+}
+
   return (
     <div>
+      <h1>users</h1>
+        <form onSubmit={handleSubmit}>
 
-      <h1>user</h1>
-      <h2>{search.get("id")}</h2>
+         <input 
+         type='text' 
+         placeholder='name'
+         value={name}  
+         onChange={(e)=> { 
+          setName(e.target.value)
+          }}/>
+
+         <button type='submit' >Find</button>
+
+        </form>
+
+      {/* <h1>user</h1>
+      <h2>{search.get("id")}</h2> */}
     </div>
   )
 }
